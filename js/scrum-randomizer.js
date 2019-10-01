de0de976ed6f75b3ff0cc8d50128ca5e7600a62a
Anselm Bradford <github@anselmbradford.com>
2019-09-30 23:04:58 -0400

    Make list from URL params

--- js/scrum-randomizer.js
+++ js/scrum-randomizer.js
@@ -1,67 +1,136 @@
-var teams = {
-  'Maintenance &amp; Response': {
-    'first': [],
-    'randomized': [
-      'Ans',
-      'Betsy',
-      'Craig',
-      'Harry',
-      'Nicholas',
-      'Scott',
-      'Sophie',
-      'Jess/Will'
-    ],
-    'last': []
-  }
-};
-today = new Date();
-
-function getNextName( team ) {
-  var n = today.getDate() + today.getMonth() + today.getFullYear(),
-      i = n % ( Object.keys(team).length ),
-      name = team[i];
-  $( '#team-order' ).append( '<li>' + name + '</li>' );
-  team.splice( i, 1 );
-};
-
-function orderTeam( team ) {
-  var n = today.getDate() + today.getMonth() + today.getFullYear(),
-      firstLength = team.first.length,
-      randomizedLength = team.randomized.length,
-      lastLength = team.last.length,
-      content = '';
-
-  for ( var x = 0; x < firstLength; x++ ) {
-    var name = team.first[x];
-    content += '<li>' + name + '</li>';
-  }
-
-  for ( var y = 0; y < randomizedLength; y++ ) {
-    var i = n % ( team.randomized.length ),
-        name = team.randomized[i];
-
-    content += '<li>' + name + '</li>';
-    team.randomized.splice( i, 1 );
-  }
-
-  for ( var z = 0; z < lastLength; z++ ) {
-    var name = team.last[z];
-    content += '<li>' + name + '</li>';
-  }
-
-  return content;
+/**
+ * Convert URL parameter key/values into a JavaScript object.
+ * @return {Object} An object of key/value URL parameter pairs.
+ */
+function getURLParams() {
+  var search = location.search.substring(1);
+  if ( search === '' ) return {};
+  return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
 }
 
-$( document ).ready( function() {
-  var dateText = ( today.getMonth() + 1 ) + '/' + today.getDate() + '/' + today.getFullYear();
-  $( '#date-today').text( dateText );
+/**
+ * @return {Array} A list of team members from the "team" parameter in the URL.
+ */
+function getTeamFromURL() {
+  var params = getURLParams()
+  if ( typeof params.team === 'undefined' ) {
+    return [];
+  }
+  
+  return params.team.split(',');
+}
 
-  for ( var key in teams ) {
-    var name = key,
-        team = teams[key],
-        content = orderTeam( team );
-    $( 'body' ).append( '<h2>' + name + '</h2>');
-    $( 'body' ).append( '<ol>' + content + '</ol>');
+/**
+ * Build the team view from the URL.
+ */
+function buildTeamView() {
+  teamViewDom.style.display = 'block';
+  var teamMember;
+  var generateOrderBtn = teamViewDom.querySelector( 'button' );
+  var teamCheckboxListDom = teamViewDom.querySelector( 'ul' );
+
+  for ( var i = 0, len = team.length; i < len; i++ ) {
+    teamMember = team[i];
+    
+    // Create list item
+    var listItemElement = document.createElement( 'li' );
+    listItemElement.innerHTML = '<label><input type="checkbox" name="team" value="' + 
+                                teamMember + '" checked>' + teamMember + '</label>';
+
+    teamCheckboxListDom.appendChild( listItemElement );
   }
 
-});
+  generateOrderBtn.addEventListener( 'click', showListView );
+}
+
+/**
+ * Build the list view from the present team members.
+ */
+function buildListView() {
+  var teamListDom = listViewDom.querySelector( 'ol' );
+  teamListDom.innerHTML = '';
+
+  var dateText = ( today.getMonth() + 1 ) + '/' + today.getDate() + '/' + today.getFullYear();
+  var dateTodayDom = document.querySelector( '#date-today' );
+  dateTodayDom.innerHTML = dateText;
+
+  var selectedTeamMembers = teamViewDom.querySelectorAll( 'li input' );
+
+  var selectedTeamList = [];
+  var selectedTeamMemberCheckbox;
+  for ( var i = 0, len = selectedTeamMembers.length; i < len; i++ ) {
+    selectedTeamMemberCheckbox = selectedTeamMembers[i];
+    if ( selectedTeamMemberCheckbox.checked ) {
+      selectedTeamList.push( selectedTeamMembers[i].value );
+    }
+  }
+  
+  selectedTeamList = shuffle( selectedTeamList );
+  var teamMember;
+  for ( var i = 0, len = selectedTeamList.length; i < len; i++ ) {
+    teamMember = selectedTeamList[i];
+    //var content = orderTeam( teamMember );
+    var listItemElement = document.createElement( 'li' );
+    listItemElement.innerHTML = teamMember;
+    teamListDom.appendChild( listItemElement );
+  }
+}
+
+/**
+ * Show the list view.
+ */
+function showListView() {
+  buildListView();
+
+  listViewDom.style.display = 'block';
+}
+
+/**
+ * Fisher-Yates Shuffle
+ * See https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+ * @param {Array} list to shuffle. 
+ */
+function shuffle(array) {
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// Get today's date.
+var today = new Date();
+
+// Reference views.
+var noticeViewDom = document.querySelector( '#notice-view' );
+var teamViewDom = document.querySelector( '#team-view' );
+var listViewDom = document.querySelector( '#list-view' );
+
+// Hide the views initially.
+teamViewDom.style.display = 'none';
+listViewDom.style.display = 'none';
+
+// Retrieve team from URL.
+var team = getTeamFromURL();
+
+if ( team.length < 1 ) {
+  noticeViewDom.innerHTML = '<h2>No team members found!</h2>';
+} else {
+  noticeViewDom.style.display = 'none';
+
+  // Build the team view.
+  buildTeamView();
+}
